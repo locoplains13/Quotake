4068f265714747e061aaac3d9908c469e059b57e
Angel Andrade <ara483@usask.ca>
2022-11-08 19:52:01 -0600

    created a basic app layout with a simple 'click me button'

---
+++ src/App.java
@@ -0,0 +1,27 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+
+public class App extends Application{
+
+    Button button;
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Quotake");
+        button = new Button("click me");
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 300, 250);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
