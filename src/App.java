38ab8d42096cf562c2eb752df540db4265dccc17
Angel Roberto Andrade <alroc1234@hotmail.com>
2022-11-26 17:34:36 -0600

    quotes now get displayed on screen with toString()

--- src/App.java
+++ src/App.java
@@ -3,15 +3,17 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
 
 public class App extends Application{
 
     Button addButton;
     Button confirmButton;
+    Stage window;
+    Boolean addedNewAuthor = false;
 
     public static void main(String[] args) throws Exception {
         launch(args);
@@ -20,58 +22,102 @@ public class App extends Application{
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+        window = primaryStage;
+
         ArrayList<Author> authorList = new ArrayList<Author>();
         ArrayList<Quotes> quoteList = new ArrayList<Quotes>();
 
-        primaryStage.setTitle("Quotake");
+        window.setTitle("Quotake");
         addButton = new Button("+");
-        Label authorLabel = new Label("Author: ");
-        Label quoteLabel = new Label("Quote: ");
-
-        TextField authorTextField = new TextField();
-        TextField quoteTextField = new TextField();
-
-        confirmButton = new Button("Confirm");
 
         /*
          * this method asks the user for author and quote to submit it
          */
         addButton.setOnAction(e -> {
             GridPane root = new GridPane();
-            root.addRow(0, authorLabel, authorTextField);
+
+            Label authorLabel = new Label("Author: ");
+            Label quoteLabel = new Label("Quote: ");
+    
+            // fields of text where the second scene will ask the user for the inputs
+            TextField authorTextField = new TextField();
+            TextField quoteTextField = new TextField();
+
+            ChoiceBox<String> authorsChoiceBox = new ChoiceBox<>();
+            
+            for(int i = 0; i < authorList.size(); i++){
+                authorsChoiceBox.getItems().add(authorList.get(i).getAuthor());
+            }
+            authorsChoiceBox.getItems().add("Add new author");
+            confirmButton = new Button("Confirm");
+    
+            root.addRow(0, authorLabel, authorsChoiceBox);
+
+            authorsChoiceBox.getSelectionModel().selectedItemProperty().addListener( (v, oldValue, newValue) -> {
+                if(newValue == "Add new author"){
+                    root.getChildren().remove(authorsChoiceBox);
+                    root.addRow(0, authorTextField);
+                }
+            });
+
             root.addRow(1, quoteLabel, quoteTextField);
             root.addRow(2, confirmButton);
+
+            if(root.getChildren().contains(authorTextField)){
+                addedNewAuthor = true;
+            }
+
             Scene quoteInputs = new Scene(root, 250, 100);
             Stage addButtonStage = new Stage();
+            
             addButtonStage.setTitle("Add Quote");
             addButtonStage.setScene(quoteInputs);
             addButtonStage.show();
-            
+            /*
+            * this method will add author and quote objects and print what the user input
+            */
             confirmButton.setOnAction(e1 -> {
-                Author a = new Author(authorTextField.getText());
-                authorList.add(a);
+                String authorText;
+                if(addedNewAuthor){
+                    authorText = authorsChoiceBox.getValue();
+                }else{
+                    authorText = authorTextField.getText();
+                }
+
+                Author a = new Author(authorText);
+
+                if(!authorList.contains(a)) authorList.add(a);
+
                 Quotes q = new Quotes(quoteTextField.getText(), a.getAuthor());
                 quoteList.add(q);
+                
                 addButtonStage.close();
-                System.out.println(q.toString());
+                
             });
         });
 
-        /*
-         * this method will add author and quote objects and print what the user input
-         */
-
+        // how the contents of the scene are ordered
         GridPane layout = new GridPane();
+        
         layout.addRow(0, addButton);
+
         Label emptyListLabel = new Label("No quotes registered at this time");
 
         if(quoteList.isEmpty()){
             layout.addColumn(1, emptyListLabel);
+        }else{
+            for(int i = 0; i < quoteList.size(); i++){
+                Label displayQuoteLabel = new Label(quoteList.get(i).getQuote());
+                layout.addColumn((i), displayQuoteLabel);
+            }
         }
 
         Scene scene = new Scene(layout, 500, 300);
 
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        // adding scene to the stage
+        window.setScene(scene);
+
+        //showing the contents of the scene
+        window.show();
     }
 }
