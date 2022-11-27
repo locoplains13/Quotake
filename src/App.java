45f16db667084d844e7a1a1a59ff81d08785dc89
Angel Roberto Andrade <alroc1234@hotmail.com>
2022-11-27 15:51:52 -0600

    works with css and kinda with vbox

--- src/App.java
+++ src/App.java
@@ -1,12 +1,16 @@
 import java.util.ArrayList;
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 
 public class App extends Application{
 
@@ -14,6 +18,7 @@ public class App extends Application{
     Button confirmButton;
     Stage window;
     Boolean addedNewAuthor = false;
+    Button collectionButton;
 
     public static void main(String[] args) throws Exception {
         launch(args);
@@ -24,11 +29,26 @@ public class App extends Application{
 
         window = primaryStage;
 
+        String css = this.getClass().getResource("app.css").toExternalForm();
+
+        VBox vbox = new VBox(10);
+        vbox.setAlignment(Pos.CENTER_LEFT);
+
+        VBox authorsBox = new VBox(10);
+        authorsBox.setAlignment(Pos.TOP_RIGHT);
+
         ArrayList<Author> authorList = new ArrayList<Author>();
         ArrayList<Quotes> quoteList = new ArrayList<Quotes>();
 
         window.setTitle("Quotake");
         addButton = new Button("+");
+        collectionButton = new Button("+C");
+
+        
+        BorderPane layout = new BorderPane();
+    
+
+        Label emptyListLabel = new Label("No quotes registered at this time");
 
         /*
          * this method asks the user for author and quote to submit it
@@ -43,29 +63,12 @@ public class App extends Application{
             TextField authorTextField = new TextField();
             TextField quoteTextField = new TextField();
 
-            ChoiceBox<String> authorsChoiceBox = new ChoiceBox<>();
-            
-            for(int i = 0; i < authorList.size(); i++){
-                authorsChoiceBox.getItems().add(authorList.get(i).getAuthor());
-            }
-            authorsChoiceBox.getItems().add("Add new author");
             confirmButton = new Button("Confirm");
     
-            root.addRow(0, authorLabel, authorsChoiceBox);
-
-            authorsChoiceBox.getSelectionModel().selectedItemProperty().addListener( (v, oldValue, newValue) -> {
-                if(newValue == "Add new author"){
-                    root.getChildren().remove(authorsChoiceBox);
-                    root.addRow(0, authorTextField);
-                }
-            });
+            root.addRow(0, authorLabel, authorTextField);
 
             root.addRow(1, quoteLabel, quoteTextField);
             root.addRow(2, confirmButton);
-
-            if(root.getChildren().contains(authorTextField)){
-                addedNewAuthor = true;
-            }
 
             Scene quoteInputs = new Scene(root, 250, 100);
             Stage addButtonStage = new Stage();
@@ -78,11 +81,7 @@ public class App extends Application{
             */
             confirmButton.setOnAction(e1 -> {
                 String authorText;
-                if(addedNewAuthor){
-                    authorText = authorsChoiceBox.getValue();
-                }else{
-                    authorText = authorTextField.getText();
-                }
+                authorText = authorTextField.getText();
 
                 Author a = new Author(authorText);
 
@@ -91,29 +90,32 @@ public class App extends Application{
                 Quotes q = new Quotes(quoteTextField.getText(), a.getAuthor());
                 quoteList.add(q);
                 
+                layout.getChildren().remove(emptyListLabel);
+
+                Label displayQuoteLabel = new Label(quoteList.get(quoteList.size()-1).toString());
+                authorsBox.getChildren().add(displayQuoteLabel);
+
                 addButtonStage.close();
                 
             });
         });
 
         // how the contents of the scene are ordered
-        GridPane layout = new GridPane();
         
-        layout.addRow(0, addButton);
 
-        Label emptyListLabel = new Label("No quotes registered at this time");
+        vbox.getChildren().addAll(addButton, collectionButton);
+        layout.setLeft(vbox);
+        layout.setCenter(authorsBox);
+        ScrollPane scrollPane = new ScrollPane();
 
-        if(quoteList.isEmpty()){
-            layout.addColumn(1, emptyListLabel);
-        }else{
-            for(int i = 0; i < quoteList.size(); i++){
-                Label displayQuoteLabel = new Label(quoteList.get(i).getQuote());
-                layout.addColumn((i), displayQuoteLabel);
-            }
-        }
+        scrollPane.setContent(layout);
 
-        Scene scene = new Scene(layout, 500, 300);
-
+        scrollPane.setVbarPolicy(ScrollBarPolicy.AS_NEEDED);
+        
+        Scene scene = new Scene(scrollPane, 1000, 800);
+        scene.getStylesheets().add(css);
+        // adds css to the app
+        
         // adding scene to the stage
         window.setScene(scene);
 
